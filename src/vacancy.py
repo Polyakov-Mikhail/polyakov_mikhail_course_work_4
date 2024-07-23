752ee47d99e5d3dadb7673989d50bd2d7c5cbc27
Mikhail Polyakov <timadgi@gmail.com>
2024-07-23 08:51:00 +0500

    change Class Vacancy

--- src/vacancy.py
+++ src/vacancy.py
@@ -1,59 +1,68 @@
+import json
+# from typing import List, Optional
+from src.API_HH import HeadHunterRuAPI
+
 
 class Vacancy:
     """ Класс для работы с вакансиями """
+    def __init__(self, name, salary, currency, url, description, requirement='Информация отсутствует'):
+        self.name = name
+        self.salary = salary
+        self.currency = currency
+        self.url = url
+        self.description = description
+        self.requirement = requirement
 
-    __slots__ = ("name", "alternate_url", "salary_from", "salary_to", "area_name", "requirement", "responsibility")
+    def __repr__(self):
+        return (f'Название вакансии: {self.name}\n'
+                f'Зарплата: {self.salary} {self.currency}\n'
+                f'Описание вакансии: {self.description}\n'
+                f'Требования: {self.requirement}\n'
+                f'Ссылка на вакансию: <{self.url}>\n')
 
-    def __init__(self, name, alternate_url, salary_from, salary_to, area_name, requirement, responsibility):
+    def __lt__(self, other):
+        """Метод сравнения вакансий между собой по зарплате и валидации данных по зарплате"""
 
-        self.name: str = name
-        self.alternate_url: str = alternate_url
-        self.salary_from: int = salary_from
-        self.salary_to: int = salary_to
-        self.area_name: str = area_name
-        self.requirement: str = requirement
-        self.responsibility: str = responsibility
+        if self.salary["from"] != 0 and other.salary["to"] != 0:
+            if self.salary['to'] < other.salary['to']:
+                return self
+            else:
+                return other
+        return 'Зарплата не указана'
 
-    def __str__(self) -> str:
-        """ Строковое представление вакансии """
+    def __str__(self):
+        """Метод для представляния вакансий при печати"""
+        return (f'{self.name}\n'
+                f'{self.url}\n'
+                f'{self.description}\n'
+                f'{self.salary["from"] if self.salary["from"] == 0 else "ЗП от не указана"} - '
+                f'{self.salary["to"] if self.salary["to"] == 0 else "ЗП до не указана"} '
+                f'{self.currency}\n')
 
-        return (f"Наименование вакансии: {self.name}\n"
-                f"Ссылка на вакансию: {self.alternate_url}\n"
-                f"Зарплата: от {self.salary_from} до {self.salary_to}\n"
-                f"Место работы: {self.area_name}\n"
-                f"Краткое описание: {self.requirement}\n"
-                f"{self.responsibility}\n")
 
-    def __lt__(self, other) -> bool:
-        """ Метод сравнения от большего к меньшему """
+    # def to_dict(self) -> dict:
+    #     """ Метод возвращает вакансию в виде словаря """
+    #
+    #     return {
+    #         "name": self.name,
+    #         "alternate_url": self.alternate_url,
+    #         "salary_from": self.salary_from,
+    #         "salary_to": self.salary_to,
+    #         "area_name": self.area_name,
+    #         "requirement": self.requirement,
+    #         "responsibility": self.responsibility,
+    #     }
 
-        return self.salary_from < other.salary_from
 
-    @classmethod
-    def from_hh_dict(cls, vacancy_data: dict):
-        """ Метод возвращает экземпляр класса в виде списка """
+# vacancies = [Vacancy.from_hh_dict(vacancy) for vacancy in vacancies]
+# vacancies = sorted(vacancies, reverse=True)
 
-        salary = vacancy_data.get("salary")
+# print("Топ выбранных вакансии с 'HeadHunter' по зарплате: \n")
+# for i in vacancies:
+#     print(i)
 
-        return cls(
-            vacancy_data["name"],
-            vacancy_data["alternate_url"],
-            salary.get("from") if salary.get("from") else 0,
-            salary.get("to") if salary.get("to") else 0,
-            vacancy_data["area"]["name"],
-            vacancy_data["snippet"]["requirement"],
-            vacancy_data["snippet"]["responsibility"],
-        )
+# vacancies = [vacancy.to_dict() for vacancy in vacancies]
 
-    def to_dict(self) -> dict:
-        """ Метод возвращает вакансию в виде словаря """
-
-        return {
-            "name": self.name,
-            "alternate_url": self.alternate_url,
-            "salary_from": self.salary_from,
-            "salary_to": self.salary_to,
-            "area_name": self.area_name,
-            "requirement": self.requirement,
-            "responsibility": self.responsibility,
-        }
+# a = Vacancy()
+#
+# print(json.dumps(a.to_dict(), ensure_ascii=False, indent=4))
