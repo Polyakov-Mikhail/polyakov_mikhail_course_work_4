360bd23afd4ca76e05d3552d52a3cbd4ed9d1112
Mikhail Polyakov <timadgi@gmail.com>
2024-07-25 00:28:21 +0500

    corrected functions.py

--- src/functions.py
+++ src/functions.py
@@ -5,21 +5,5 @@ from typing import List
 
 
 def top_sort_vac(all_vac: List[Vacancy], top_n: int) -> List[Vacancy]:
+    all_vac = sorted(all_vac, reverse=True)
     return all_vac[:top_n]
-
-
-a = HeadHunterRuAPI()
-vacancies = a.getting_vacancies("курьер")
-vaca = a.validate_data(vacancies)
-
-vacanc = JSONSaver()
-vacanc.add_data(vaca)
-
-
-my_vac = JSONSaver()
-vac_data = Vacancy.cast_to_object_list(my_vac.get_data())
-vac_data = sorted(vac_data, reverse=True)
-top_n_vac = top_sort_vac(vac_data, 5)
-
-for w in top_n_vac:
-    print(w)
