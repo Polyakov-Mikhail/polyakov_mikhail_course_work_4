9d67ae4822ac527a30402bed4684dde2b942fdda
Mikhail Polyakov <timadgi@gmail.com>
2024-07-25 09:20:11 +0500

    corrected vacancies.json and add config.py

--- src/vacancy_json.py
+++ src/vacancy_json.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from config import path_operations
 import os
 import json
 
@@ -20,20 +21,20 @@ class Saver(ABC):
 
 
 class JSONSaver(Saver):
-    """ Класс для записи в json-файл по пути data/vacancies.json"""
+    """ Класс для записи в json-файл по пути data/vacancies.json (файл config)"""
 
-    data_json = "data/vacancies.json"
-    path_operations = os.path.abspath(data_json)
+    def __init__(self, filepath: str = path_operations):
+        self.__filepath = filepath
 
     def add_data(self, vacancy):
         """Сохранить все вакансии в файл"""
-        print(f'\nЯ нашел {len(vacancy)} подходящих вакансий и сохранил в файл {self.path_operations}\n\n')
-        with open(self.path_operations, 'w', encoding='utf-8') as file:
+        print(f'\nЯ нашел {len(vacancy)} подходящих вакансий и сохранил в файл {self.__filepath}\n\n')
+        with open(self.__filepath, 'w', encoding='utf-8') as file:
             json.dump(vacancy, file, ensure_ascii=False, indent=4)
 
     def get_data(self):
         """ Получение данных json из файла"""
-        with open(self.path_operations, encoding='utf-8') as file:
+        with open(self.__filepath, encoding='utf-8') as file:
             return json.loads(file.read())
 
     def del_data(self):
